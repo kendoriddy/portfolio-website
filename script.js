29d095f77fcc07d4fb9d03bd93d02b4e7f03a422
Kehinde Onifade <onifkay@gmail.com>
2022-07-18 20:56:13 +0100

    Fix Linters errors

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const closeIcon = document.getElementById('close-icon-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileList = document.getElementById('mobile-menu-list');
 
-hamburger.addEventListener("click", function() {
-    mobileMenu.style.display = 'block';
-})
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+});
 
-closeIcon.addEventListener("click", function() {
-    mobileMenu.style.display = 'none';
-})
+closeIcon.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+});
 
-mobileList.addEventListener("click", function() {
-    mobileMenu.style.display = 'none'
-})
+mobileList.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+});
