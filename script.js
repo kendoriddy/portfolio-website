679b78d4a807b074a627228289856008223491ca
Kehinde Onifade <onifkay@gmail.com>
2022-07-18 20:37:42 +0100

    Add javascript functionalities to hamburger on mobile view

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.getElementById('hamburger');
+const closeIcon = document.getElementById('close-icon-btn');
+
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileList = document.getElementById('mobile-menu-list');
 
 hamburger.addEventListener("click", function() {
-    
+    mobileMenu.style.display = 'block';
+})
+
+closeIcon.addEventListener("click", function() {
+    mobileMenu.style.display = 'none';
+})
+
+mobileList.addEventListener("click", function() {
+    mobileMenu.style.display = 'none'
 })
