7b09dc5a65743e53a0f5ee23d94f76d0ce5864ca
Kehinde Onifade <onifkay@gmail.com>
2022-07-21 15:57:18 +0100

    Create function to retrive data from LocalStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -8,6 +8,15 @@ function populateStorage() {
   formData.name = document.getElementById('name').value;
   formData.email = document.getElementById('email').value;
   formData.message = document.getElementById('message').value;
-  
+
   localStorage.setItem('form-data', JSON.stringify(formData));
+}
+
+function getData() {
+  let storageData = localStorage.getItem('form-data');
+  storageData = JSON.parse(storageData);
+
+  document.getElementById('name').value = storageData.name;
+  document.getElementById('email').value = storageData.email;
+  document.getElementById('message').value = storageData.message;
 }
