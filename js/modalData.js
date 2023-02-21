0c8409c48f538bd99b8e43ba56d83663d2d606b2
Kehinde Onifade <onifkay@gmail.com>
2023-02-21 22:13:55 +0100

    update modalData

--- js/modalData.js
+++ js/modalData.js
@@ -6,7 +6,7 @@ const projectData = [
     live: 'https://kendoriddy.github.io/awesome-books-v2/',
     source: 'https://github.com/kendoriddy/awesome-books-v2',
     description:
-      'ExpertHub is a technician reservation website that provide the public with an easy and convenient way to rent technicians and offer technical services to people..',
+      'ExpertHub is a technician reservation website that provides the public with an easy and convenient way to rent technicians and offer technical services to people.',
   },
   {
     snapshot: './assets/images/awesome.png',
@@ -18,22 +18,13 @@ const projectData = [
       'This project is a website that displays a list of books that users have added.It allows you to add and remove books from that list also. The goal of this project is to build a simple multi-page responsive book website.',
   },
   {
-    snapshot: './assets/images/box.png',
-    projectName: 'BoxMonie Web Application',
-    technologies: ['React', 'JavaScript', 'Node', 'CSS'],
-    live: 'http://boxmonie.com/',
-    source: 'https://github.com/TIIDELab',
-    description:
-      'This project is a group project where I collaborated with some colleagues to build a fullstack fin-tech web application(github repository is private). It is an online peer-to-peer, crowd-sourcing platform. It operates as an online community that enables groups of people such as partners, friends or strangers to come together to pool money and take turns receiving the total amount contributed.',
-  },
-  {
     snapshot: './assets/images/sc1.png',
     projectName: 'Stuk Mart',
     technologies: ['React', 'JavaScript', 'Redux', 'API'],
     live: 'https://stuk-mart.netlify.app/',
     source: 'https://github.com/kendoriddy/stuk-mart',
     description:
-    'Stuk-Mart is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics. Its data is consumed from the Financial modelling API. It is built using React, Redux, and React-bootstrap, Skeleton Loader, Recharts.',
+      'Stuk-Mart is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics. Its data is consumed from the Financial modelling API. It is built using React, Redux, and React-bootstrap, Skeleton Loader, Recharts.',
   },
   {
     snapshot: './assets/images/math2.png',
@@ -53,7 +44,7 @@ const projectData = [
     live: 'https://kendoriddy.github.io/BeefLand-Eatery/',
     source: 'https://github.com/kendoriddy/BeefLand-Eatery',
     description:
-      '"Math-Magician" is a web app for all lovers of mathematics. It is a Single Page Application (SPA) that allows users to make simple calculations, generate and read random math-related quotes and also tweet the quote.',
+      'The BeefLand web application displays a list of meals that were provided by an external API. The users can like a meal, leave some comments or make a reservation, in these cases an involvement API was used. N.B. The reservation functions are not implemented because there are just two in the group.',
   },
   {
     snapshot: './assets/images/app-img.png',
@@ -62,7 +53,7 @@ const projectData = [
     live: 'https://a-bookstore.netlify.app/',
     source: 'https://github.com/kendoriddy/Bookstore',
     description:
-    'The Bookstore is a website where the user can display a list of books, add a book by providing a title, an author, and selecting from the categories, and remove a selected book.',
+      'The Bookstore is a website where the user can display a list of books, add a book by providing a title, an author, and selecting from the categories, and remove a selected book.',
   },
   {
     snapshot: './assets/images/travel.png',
@@ -71,7 +62,7 @@ const projectData = [
     live: 'https://marvel-space.netlify.app/',
     source: 'https://github.com/kendoriddy/Space-Travelers-Hub',
     description:
-    'The Space Travelers is a a React, Redux application based on the SpaceX API. This app is built with HTML | CSS | JAVASCRIPT | React and Redux and JEST it uses multiple API to render rockets and missions and enable users to have reserve the reocket and also join the mission.',
+      'The Space Travelers is a a React, Redux application based on the SpaceX API. This app is built with HTML | CSS | JAVASCRIPT | React and Redux and JEST it uses multiple API to render rockets and missions and enable users to have reserve the reocket and also join the mission.',
   },
   {
     snapshot: './assets/images/pictury.png',
@@ -88,8 +79,7 @@ const projectData = [
     technologies: ['React', 'JavaScript', 'Linters', 'CSS'],
     live: 'https://asostracker.netlify.app/',
     source: 'https://github.com/kendoriddy/asos-birthday-tracker',
-    description:
-      'I created a webapp that tracks the birthdays of my secondary school classmates.',
+    description: 'I created a webapp that tracks the birthdays of my secondary school classmates.',
   },
 ];
 
