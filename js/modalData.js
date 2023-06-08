082433f6b6c7edb96edad669a01c7d267d819835
Kehinde Onifade <onifkay@gmail.com>
2023-06-08 11:23:23 +0100

    update modalData

--- js/modalData.js
+++ js/modalData.js
@@ -1,12 +1,49 @@
 const projectData = [
+  // {
+  //   snapshot: './assets/images/expert4.png',
+  //   projectName: 'ExpertHub',
+  //   technologies: ['React JS', 'Ruby on Rails', 'Linters', 'SCSS'],
+  //   live: 'https://homeofexperts.netlify.app/',
+  //   source: 'https://github.com/kendoriddy/experts_hub',
+  //   description:
+  // eslint-disable-next-line max-len
+  //     'ExpertHub is a technician reservation website that provides the public with an easy and convenient way to rent technicians and offer technical services to people.',
+  // },
   {
-    snapshot: './assets/images/expert4.png',
-    projectName: 'ExpertHub',
-    technologies: ['React JS', 'Ruby on Rails', 'Linters', 'SCSS'],
-    live: 'https://homeofexperts.netlify.app/',
-    source: 'https://github.com/kendoriddy/experts_hub',
+    snapshot: './assets/images/picture.png',
+    projectName: 'Pictury',
+    technologies: ['React', 'JavaScript', 'API', 'Tailwind CSS'],
+    live: 'https://pictury.netlify.app/',
+    source: '',
     description:
-      'ExpertHub is a technician reservation website that provides the public with an easy and convenient way to rent technicians and offer technical services to people.',
+      'Pictury is a dynamic social media platform designed for seamless picture sharing and engaging interactions. It empowers users to effortlessly upload and download captivating images while fostering meaningful connections within a vibrant community of fellow photo enthusiasts.',
+  },
+  {
+    snapshot: './assets/images/math2.png',
+    desktopSnapshot: './assets/images/math1.png',
+    projectName: 'Math-Magician & QR Code Generator',
+    technologies: ['React', 'JavaScript', 'CSS', 'Linters'],
+    live: 'https://mathe-magic.netlify.app/',
+    source: 'https://github.com/kendoriddy/math-magicians',
+    description:
+      '"Math-Magician" started as just a web app for all lovers of mathematics. It is a Single Page Application (SPA) that allows users to make simple calculations, generate and read random math-related quotes and also tweet the quote. But now, it is more than just a calculator, it has a roman numeral converter and a QR code generator.',
+  },
+  {
+    snapshot: './assets/images/asos.png',
+    projectName: 'ASOS Birthday Tracker',
+    technologies: ['React', 'JavaScript', 'Linters', 'CSS'],
+    live: 'https://asostracker.netlify.app/',
+    source: 'https://github.com/kendoriddy/asos-birthday-tracker',
+    description: 'I created a webapp that tracks the birthdays of my secondary school classmates.',
+  },
+  {
+    snapshot: './assets/images/pictury.png',
+    projectName: 'Picture World',
+    technologies: ['React', 'JavaScript', 'API', 'Tailwind CSS'],
+    live: 'https://pictureworld.netlify.app/',
+    source: 'https://github.com/kendoriddy/picture-world',
+    description:
+      'I built an image gallery app using the Pixabay API, by integrating Tailwind CSS with React.',
   },
   {
     snapshot: './assets/images/awesome.png',
@@ -18,23 +55,13 @@ const projectData = [
       'This project is a website that displays a list of books that users have added.It allows you to add and remove books from that list also. The goal of this project is to build a simple multi-page responsive book website.',
   },
   {
-    snapshot: './assets/images/sc1.png',
-    projectName: 'Stuk Mart',
-    technologies: ['React', 'JavaScript', 'Redux', 'API'],
-    live: 'https://stuk-mart.netlify.app/',
-    source: 'https://github.com/kendoriddy/stuk-mart',
+    snapshot: './assets/images/travel.png',
+    projectName: 'Marvel Space Hub',
+    technologies: ['React', 'Redux', 'Rest API', 'Linters'],
+    live: 'https://marvel-space.netlify.app/',
+    source: 'https://github.com/kendoriddy/Space-Travelers-Hub',
     description:
-      'Stuk-Mart is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics. Its data is consumed from the Financial modelling API. It is built using React, Redux, and React-bootstrap, Skeleton Loader, Recharts.',
-  },
-  {
-    snapshot: './assets/images/math2.png',
-    desktopSnapshot: './assets/images/math1.png',
-    projectName: 'Math-Magician',
-    technologies: ['React', 'JavaScript', 'CSS', 'Linters'],
-    live: 'https://mathe-magic.netlify.app/',
-    source: 'https://github.com/kendoriddy/math-magicians',
-    description:
-      '"Math-Magician" is a web app for all lovers of mathematics. It is a Single Page Application (SPA) that allows users to make simple calculations, generate and read random math-related quotes and also tweet the quote.',
+      'The Space Travelers is a a React, Redux application based on the SpaceX API. This app is built with HTML | CSS | JAVASCRIPT | React and Redux and JEST it uses multiple API to render rockets and missions and enable users to have reserve the reocket and also join the mission.',
   },
   {
     snapshot: './assets/images/beef.png',
@@ -47,6 +74,15 @@ const projectData = [
       'The BeefLand web application displays a list of meals that were provided by an external API. The users can like a meal, leave some comments or make a reservation, in these cases an involvement API was used. N.B. The reservation functions are not implemented because there are just two in the group.',
   },
   {
+    snapshot: './assets/images/sc1.png',
+    projectName: 'Stuk Mart',
+    technologies: ['React', 'JavaScript', 'Redux', 'API'],
+    live: 'https://stuk-mart.netlify.app/',
+    source: 'https://github.com/kendoriddy/stuk-mart',
+    description:
+      'Stuk-Mart is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics. Its data is consumed from the Financial modelling API. It is built using React, Redux, and React-bootstrap, Skeleton Loader, Recharts.',
+  },
+  {
     snapshot: './assets/images/app-img.png',
     projectName: 'Book Store',
     technologies: ['React', 'Redux', 'Rest API', 'Linters'],
@@ -54,32 +90,6 @@ const projectData = [
     source: 'https://github.com/kendoriddy/Bookstore',
     description:
       'The Bookstore is a website where the user can display a list of books, add a book by providing a title, an author, and selecting from the categories, and remove a selected book.',
-  },
-  {
-    snapshot: './assets/images/travel.png',
-    projectName: 'Marvel Space Hub',
-    technologies: ['React', 'Redux', 'Rest API', 'Linters'],
-    live: 'https://marvel-space.netlify.app/',
-    source: 'https://github.com/kendoriddy/Space-Travelers-Hub',
-    description:
-      'The Space Travelers is a a React, Redux application based on the SpaceX API. This app is built with HTML | CSS | JAVASCRIPT | React and Redux and JEST it uses multiple API to render rockets and missions and enable users to have reserve the reocket and also join the mission.',
-  },
-  {
-    snapshot: './assets/images/pictury.png',
-    projectName: 'Picture World',
-    technologies: ['React', 'JavaScript', 'API', 'Tailwind CSS'],
-    live: 'https://pictureworld.netlify.app/',
-    source: 'https://github.com/kendoriddy/picture-world',
-    description:
-      'I built an image gallery app using the Pixabay API, by integrating Tailwind CSS with React.',
-  },
-  {
-    snapshot: './assets/images/asos.png',
-    projectName: 'ASOS Birthday Tracker',
-    technologies: ['React', 'JavaScript', 'Linters', 'CSS'],
-    live: 'https://asostracker.netlify.app/',
-    source: 'https://github.com/kendoriddy/asos-birthday-tracker',
-    description: 'I created a webapp that tracks the birthdays of my secondary school classmates.',
   },
 ];
 
